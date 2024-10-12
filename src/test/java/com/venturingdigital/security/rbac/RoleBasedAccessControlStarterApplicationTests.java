99b6160c0eb600dc4394e9fd7edf0f365f931301
sanjay-prajapat <sanjay451224@gmail.com>
2024-10-12 16:57:38 +0530

    chore:add some changes

---
+++ src/test/java/com/venturingdigital/security/rbac/RoleBasedAccessControlStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.venturingdigital.security.rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleBasedAccessControlStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
